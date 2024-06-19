3fe4a9e1f6470e0a76ed04ddb30c6b72725ed94f
shashanksaurav21 <shashanksaurav21@gmail.com>
2024-06-19 17:46:10 +0530

    Hero section
    Hero section complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Highlights from "./components/Highlights";
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
+};
+
+export default App;
